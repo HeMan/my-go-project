3a84461fc97e64624f798f6c8678af115f78ba33
Jimmy Hedman <jimmy.hedman@gmail.com>
2025-04-14 15:54:54 +0200

    Deploys

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,16 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
+resource "digitalocean_database_cluster" "my_go_project_db" {
+  name       = "my-go-project-db"
+  engine     = "PG"
+  version    = "17"
+  size       = "db-s-1vcpu-1gb"
+  region     = "ams3"
+  node_count = 1
+
+}
+
 resource "digitalocean_app" "my_go_project_app" {
   spec {
     name   = "my-go-project-app"
@@ -29,6 +39,45 @@ resource "digitalocean_app" "my_go_project_app" {
         tag           = "latest"
         registry_type = "GHCR"
       }
+
+      env {
+        key   = "POSTGRES_HOSTNAME"
+        value = "$${my-go-project-db.HOSTNAME}"
+      }
+
+      env {
+        key   = "POSTGRES_PORT"
+        value = "$${my-go-project-db.PORT}"
+      }
+
+      env {
+        key   = "POSTGRES_USER"
+        value = "$${my-go-project-db.USERNAME}"
+      }
+
+      env {
+        key   = "POSTGRES_PASSWORD"
+        value = "$${my-go-project-db.PASSWORD}"
+      }
+
+      env {
+        key   = "POSTGRES_DB"
+        value = "$${my-go-project-db.DATABASE}"
+      }
+      
+      env {
+        key = "POSTGRES_SSLMODE"
+        value = "prefer"
+      }
+
+      env {
+        key = "POSTGRES_TIMEZONE"
+        value = "Europe/Stockholm"
+      }
+    }
+
+    database {
+      name    = "my-go-project-db"
     }
   }
 }
